cc4884e6747706fc8dc5c068561ce99371a7e8ea
Kushaga Bansal <99394738+Kushagrattn@users.noreply.github.com>
2025-09-05 00:04:18 +0530

    Create InstallLinuxComponent.sh

---
+++ Scripts/InstallLinuxComponent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Install CloudWatch Agent
+sudo yum install amazon-cloudwatch-agent -y
+
+# Configure CloudWatch Agent
+aws s3 cp s3://<bucket-name>/demo/config.json .           # S3 Bucket URI
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/ec2-user/config.json
+
+# Check CloudWatch Agent status
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
+
+# # Cleanup installer
+rm -f config.json
